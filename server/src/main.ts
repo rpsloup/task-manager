c929194910033f9c142b5316682f2a610203cc31
rpsloup <robin.patrik.sloup@proton.me>
2022-11-03 12:01:44 +0100

    Adding and setting up Express

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+const DEFAULT_PORT: number = 3001;
+const port = process.env.PORT || DEFAULT_PORT;
+
+app.get('/', (_, res) => res.send('It works.'));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
