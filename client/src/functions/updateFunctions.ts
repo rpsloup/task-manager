6e063cf5a6c1c4736ded81614ab175a9b54483ef
rpsloup <robin.patrik.sloup@proton.me>
2022-11-04 09:36:01 +0100

    FE: Adding streaks functionality

--- client/src/functions/updateFunctions.ts
+++ client/src/functions/updateFunctions.ts
@@ -1,6 +1,22 @@
+import type { Streak } from '../../../typings/streakTypes';
 import type { Todo } from '../../../typings/todoTypes';
 
 const API_ENDPOINT: string = 'http://localhost:3001';
+
+export const addStreak = async (streak: Omit<Streak, 'streak_id' | 'days'>, startDate: string) => {
+  const res = await fetch(`${API_ENDPOINT}/streak`, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: streak.title,
+      startDate: startDate,
+    }),
+  });
+  const newStreak = await res.json();
+  return newStreak ?? null;
+}
 
 export const updateTodo = async (todo: Todo): Promise<Todo | null> => {
   const res = await fetch(`${API_ENDPOINT}/todo`, {
@@ -14,6 +30,6 @@ export const updateTodo = async (todo: Todo): Promise<Todo | null> => {
       done: todo.done,
     }),
   });
-  const newTodo = await res.json();
-  return newTodo ?? [];
+  const updatedTodo = await res.json();
+  return updatedTodo ?? null;
 }
--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -1,10 +1,47 @@
-import DefaultLayout from '../layouts/DefaultLayout';
+import { useRef } from 'react';
 
-const AdminPage = (): JSX.Element =>
-  (
+import DefaultLayout from '../layouts/DefaultLayout';
+import { addStreak } from '../functions/updateFunctions';
+
+import type { FormEvent } from 'react';
+
+const AdminPage = (): JSX.Element => {
+  const streakTitleRef = useRef<HTMLInputElement | null>(null);
+  const streakDateRef = useRef<HTMLInputElement | null>(null);
+  
+  const handleAddStreak = (e: FormEvent) => {
+    e.preventDefault();
+    if (!streakTitleRef.current || !streakDateRef.current) return;
+    addStreak({
+      title: streakTitleRef.current.value,
+    }, streakDateRef.current.value)
+  }
+
+  const handleAddTodo = (e: FormEvent) => {
+    e.preventDefault();
+  }
+  
+  return (
     <DefaultLayout>
       <h1>Admin</h1>
+      <h2>Add a new streak</h2>
+      <form onSubmit={handleAddStreak}>
+        <input
+          type="text"
+          name="title"
+          ref={streakTitleRef}
+        />
+        <input
+          type="date"
+          name="start"
+          ref={streakDateRef}
+        />
+        <button>Add streak</button>
+      </form>
+      <h2>Add a new todo</h2>
+      <form onSubmit={handleAddTodo}></form>
     </DefaultLayout>
   );
+}
 
 export default AdminPage;
