0e46f3d855833c0c11c86e1ae450004c1a265a99
rpsloup <robin.patrik.sloup@proton.me>
2022-11-12 16:18:40 +0100

    FE: Creating the Icon element

---
+++ client/src/components/elements/Icon/Icon.component.tsx
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+import type { IconName, IconPrefix } from '@fortawesome/fontawesome-common-types';
+
+type Props = {
+  name: IconName;
+  type: IconPrefix;
+}
+
+library.add(fas);
+library.add(fab);
+
+const Icon = ({ name, type }: Props): JSX.Element =>
+  (
+    <div style={{
+      display: 'inline',
+    }}>
+      <FontAwesomeIcon icon={[type, name]} />
+    </div>
+  );
+
+export default Icon;
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import DefaultLayout from '../layouts/DefaultLayout';
+import Icon from '../components/elements/Icon';
 
 const HomePage = (): JSX.Element =>
   (
     <DefaultLayout>
       <h1>Home</h1>
+      <Icon name="paper-plane" type="fas" />
     </DefaultLayout>
   );
 
